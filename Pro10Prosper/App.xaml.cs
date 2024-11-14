934c54d87ab054712e346da2b4b798fc0b08aaab
Alfferpla <planas.afz@gmail.com>
2024-11-14 13:49:37 +0100

    222

---
+++ Pro10Prosper/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Pro10Prosper
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Pro10Prosper/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pro10Prosper
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
